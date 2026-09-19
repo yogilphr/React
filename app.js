4197838f0e66589a88597c71f6a9bb025dd29b48
Yogesh <bhandari.yogesh1992@gmail.com>
2026-09-19 16:09:33 +0530

    React-first-app

--- app.js
+++ app.js
@@ -1,3 +1,77 @@
-const heading = React.createElement("h1", {}, "Hello World From React");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="/food.png" />
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Header1 = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="/food.png" />
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <h3>Meghana Foods</h3>
+    </div>
+  );
+};
+
+
+// const Body = () => {
+//   return(
+//     <div className="body">
+//       <div className="search">search</div>
+//       <div className="res-container">
+//         <RestaurantCard />
+//       </div>
+//     </div>
+//   );
+// };
+
+
+function AppLayout() {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root")
+);
+
+root.render(<AppLayout />);
---
+++ dist/React.7c0ccee6.js
@@ -0,0 +1,5 @@
+const heading = React.createElement("h1", {}, "Hello World From React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=React.7c0ccee6.js.map
